41a8e2ca5b80685ecd3a04c2bfe5b8c50b0a5c3f
Srushti Kulkarni <kulkarnisrushti101@gmail.com>
2025-12-29 19:46:47 +0530

    Aborted last work

---
+++ backend/types/express.d.ts
@@ -0,0 +1,14 @@
+import { IUser } from "../models/User";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId: string;
+        role: string;
+      };
+    }
+  }
+}
+
+export {};
